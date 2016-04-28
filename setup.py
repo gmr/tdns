6c3071dc390a1f201d7c0a12711fd42af5224240
Gavin M. Roy <gavinr@aweber.com>
2016-04-28 19:27:09 -0400

    Update the trove classifiers and keywords

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ classifiers = ['Development Status :: 3 - Alpha',
                'Intended Audience :: Developers',
                'License :: OSI Approved :: BSD License',
                'Operating System :: OS Independent',
+               'Natural Language :: English',
                'Programming Language :: Python :: 2',
                'Programming Language :: Python :: 2.7',
                'Programming Language :: Python :: 3',
@@ -12,7 +13,9 @@ classifiers = ['Development Status :: 3 - Alpha',
                'Programming Language :: Python :: 3.5',
                'Programming Language :: Python :: Implementation :: CPython'
                'Topic :: Communications', 'Topic :: Internet',
-               'Topic :: Software Development :: Libraries']
+               'Topic :: Internet :: Name Service (DNS)',
+               'Topic :: Software Development :: Libraries',
+               'Topic :: Software Development :: Libraries :: Python Modules']
 
 requirements = ['pycares>=1,<2', 'tornado>=4.0']
 tests_require = ['nose', 'mock', 'codecov', 'coverage']
@@ -23,6 +26,7 @@ setuptools.setup(name='tdns',
                  long_description=open('README.rst').read(),
                  author='Gavin M. Roy',
                  author_email='gavinmroy@gmail.com',
+                 keywords="tornado dns asynchronous",
                  url='http://github.com/gmr/tdns',
                  py_modules=['tdns'],
                  package_data={'': ['LICENSE', 'README.rst']},
